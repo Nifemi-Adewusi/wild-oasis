880316f8aa997ded115f5fd29f41ca7e92007431
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-08 06:58:11 +0100

    Update the formatCurrency function to use the Naira

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24,6 +24,6 @@ export const getToday = function (options = {}) {
 };
 
 export const formatCurrency = (value) =>
-  new Intl.NumberFormat("en", { style: "currency", currency: "NGN" }).format(
+  new Intl.NumberFormat("en-NG", { style: "currency", currency: "NGN" }).format(
     value
   );
