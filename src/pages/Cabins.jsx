33c3ebd715fc7b51cee9cab59f7b897f913cbc5a
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-09 15:43:39 +0100

    Update Cabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-
+// import { getCabins } from "../services/apiCabins";
+// import CabinTable from "../ui/Table";
+import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+  const toggleForm = () => {
+    setShowForm((prev) => !prev);
+  };
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter/Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+      <Button onClick={toggleForm}>
+        {showForm ? "Close Form" : "Add New Cabin"}
+      </Button>
+      {showForm && <CreateCabinForm />}
+    </>
   );
 }
 
