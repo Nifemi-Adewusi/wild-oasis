425036a574f0de4c8899402ef0afb6df8eef7ae0
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-12 08:11:37 +0100

    Update CabinTable

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -42,7 +42,7 @@ function CabinTable() {
   return (
     <Table role="table">
       <TableHeader role="row">
-        <div></div>
+        <div>Image</div>
         <div>Cabin</div>
         <div>Capacity</div>
         <div>Price</div>
