4342c59674920cdec3a7897a4f268c8babec9c6d
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-12 16:22:16 +0100

    Update CreateCabinForm

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -143,7 +143,9 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
       <StyledFormRow label="Cabin photo">
         <FileInput
-          {...register("image", { required: "This field is required" })}
+          {...register("image", {
+            required: isEditSession ? false : "This field is required",
+          })}
           id="image"
           accept="image/*"
           disabled={isAdding}
@@ -155,7 +157,9 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button disabled={isAdding}>Edit cabin</Button>
+        <Button disabled={isAdding}>
+          {isEditSession ? "Edit cabin" : "Create new cabin"}
+        </Button>
       </StyledFormRow>
     </Form>
   );
