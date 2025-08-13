e60f98be2540db8bfeeaadd55bbae4235e4cae03
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-13 10:39:06 +0100

    Abstract Query Logic into its own custom hook

---
+++ src/features/cabins/useCabins.js
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCabins } from "../../services/apiCabins";
+
+export function useCabins() {
+     const {
+        data: cabins,
+        isLoading,
+        error,
+      } = useQuery({
+        queryKey: ["Cabin"],
+        queryFn: getCabins,
+      });
+      return { cabins, isLoading, error };
+}
