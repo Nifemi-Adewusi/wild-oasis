26bd118d547555c962f4094e0113db8b65a4daf5
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-13 13:00:04 +0100

    Import data coming from the useSettings hook and wiring them up with the Form as default Values

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,44 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+/* eslint-disable no-unused-vars */
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import Spinner from "../../ui/Spinner";
+import { useSettings } from "./useSettings";
+import { formatCurrency } from "../../utils/helpers";
 
 function UpdateSettingsForm() {
+  const { settings, isLoading, error } = useSettings();
+  const {
+    id,
+    minBookingLength,
+    maxBookingLength,
+    maxGuestsPerBooking,
+    breakFastPrice,
+  } = settings || {};
+  // console.log(settings);
+  // console.log(formatCurrency(breakFastPrice));
+  if (isLoading) return <Spinner />;
   return (
     <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+      <FormRow label="Minimum nights/booking">
+        <Input type="number" defaultValue={minBookingLength} id="min-nights" />
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+      <FormRow label="Maximum nights/booking">
+        <Input type="number" defaultValue={maxBookingLength} id="max-nights" />
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+      <FormRow label="Maximum guests/booking">
+        <Input
+          type="number"
+          defaultValue={maxGuestsPerBooking}
+          id="max-guests"
+        />
       </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+      <FormRow label="Breakfast price">
+        <Input
+          type="number"
+          defaultValue={formatCurrency(breakFastPrice)}
+          id="breakfast-price"
+        />
       </FormRow>
     </Form>
   );
