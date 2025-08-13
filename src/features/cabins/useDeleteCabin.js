f906ac6a6d78e267cdfa6a9812c601d7f2aec2ea
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-13 10:39:52 +0100

    Update the useDeleteCabin hook

--- src/features/cabins/useDeleteCabin.js
+++ src/features/cabins/useDeleteCabin.js
@@ -2,6 +2,7 @@ import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { deleteCabin as deleteCabinApi } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 
+
 export function useDeleteCabin() {
   // QueryClient is used to manage the cache and refetch data after mutations
   // It allows us to invalidate queries and refetch data when a mutation occurs
