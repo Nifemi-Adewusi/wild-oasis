10c9631da066464b45d78db65023e3ba445ba4a0
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-13 10:40:27 +0100

    Update the useCreateCabin

--- src/features/cabins/useCreateCabin.js
+++ src/features/cabins/useCreateCabin.js
@@ -1,20 +1,20 @@
 import { useQueryClient, useMutation,  } from "@tanstack/react-query";
 import { createCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
-import { useForm } from "react-hook-form";
+// import { useForm } from "react-hook-form";
 export function useCreateCabin() {
     const queryClient = useQueryClient();
-    const {reset} = useForm()
-        const { mutate: create, isLoading: isAdding } = useMutation({
+    // const {reset} = useForm()
+        const { mutate: create, isLoading: isCreating } = useMutation({
           mutationFn: createCabin,
           onSuccess: () => {
             queryClient.invalidateQueries({
               queryKey: ["Cabin"],
             });
             toast.success("Cabin Added Successfully");
-            reset();
+            // reset();
           },
           onError: (error) => toast.error(error.message),
         });
-    return { create, isAdding };
+    return { create, isCreating };
 }
