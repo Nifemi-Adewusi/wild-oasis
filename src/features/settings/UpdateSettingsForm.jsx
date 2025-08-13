239f35a12222ffba282eb4880bc0cd33ceadea7c
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-13 14:46:20 +0100

    Update the UpdateSettingsForm to include the React Form Hook and wire it up to receive the data

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -4,31 +4,53 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Spinner from "../../ui/Spinner";
 import { useSettings } from "./useSettings";
+import { useForm } from "react-hook-form";
 import { formatCurrency } from "../../utils/helpers";
+import { updateSetting } from "../../services/apiSettings";
+import { useUpdateSettings } from "./useUpdateSettings";
+import Button from "../../ui/Button";
 
 function UpdateSettingsForm() {
   const { settings, isLoading, error } = useSettings();
   const {
-    id,
     minBookingLength,
     maxBookingLength,
     maxGuestsPerBooking,
     breakFastPrice,
   } = settings || {};
-  // console.log(settings);
-  // console.log(formatCurrency(breakFastPrice));
+  const { register, handleSubmit, formState } = useForm();
+
+  const { errors } = formState;
+  const { updateSettings, isUpdatingSetting } = useUpdateSettings();
+  const onSubmit = (data) => {
+    console.log(data);
+    updateSettings(data);
+  };
   if (isLoading) return <Spinner />;
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" defaultValue={minBookingLength} id="min-nights" />
+        <Input
+          type="number"
+          {...register("minBookingLength")}
+          defaultValue={minBookingLength}
+          id="min-nights"
+          disabled={isUpdatingSetting}
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" defaultValue={maxBookingLength} id="max-nights" />
+        <Input
+          {...register("maxBookingLength")}
+          type="number"
+          defaultValue={maxBookingLength}
+          id="max-nights"
+          disabled={isUpdatingSetting}
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
+          {...register("maxGuestsPerBooking")}
           defaultValue={maxGuestsPerBooking}
           id="max-guests"
         />
@@ -36,10 +58,15 @@ function UpdateSettingsForm() {
       <FormRow label="Breakfast price">
         <Input
           type="number"
-          defaultValue={formatCurrency(breakFastPrice)}
+          {...register("breakFastPrice")}
+          defaultValue={breakFastPrice}
           id="breakfast-price"
+          disabled={isUpdatingSetting}
         />
       </FormRow>
+      <Button type="submit" disabled={isUpdatingSetting}>
+        Update Settings
+      </Button>
     </Form>
   );
 }
