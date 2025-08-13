538361c4349535763a1fcef747a52e1018abf2db
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-13 15:47:10 +0100

    Update the useSettings

--- src/features/settings/useSettings.js
+++ src/features/settings/useSettings.js
@@ -2,15 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { getSettings } from "../../services/apiSettings";
 
 export function useSettings() {
-    // const queryClient = useQueryClient();
-    // const {mutate:}
-
-    const { data: settings, isLoading, error } = useQuery(
-        {
-            queryKey: ["settings"],
-            queryFn: getSettings,
-        }
-    )
-    return {settings, isLoading, error}
+  const {
+    data: settings,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["settings"],
+    queryFn: getSettings,
+  });
+  return { settings, isLoading, error };
 }
 
