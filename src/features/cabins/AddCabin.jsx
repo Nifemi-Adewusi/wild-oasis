24af8e9dddf733f623744153a99ac788752743be
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-18 17:51:19 +0100

    Update AddCabin to use the closeodal functionality

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -4,18 +4,23 @@ import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
 const AddCabin = () => {
-     const [showModal, setShowModal] = useState(false);
-  return (
+    const [showModal, setShowModal] = useState(false);
+    const closeModal = () => setShowModal(false);
+    return (
       <div>
-           <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-                  <Button onClick={() => setShowModal(!showModal)}>
-                    {showModal ? "Close Form" : "Add New Cabin"}
-                  </Button>
-          </div>
-          {showModal && <Modal>
-             <CreateCabinForm/>
-          </Modal>}
-    </div>
-  )
+        <div
+          style={{ width: "100%", display: "flex", justifyContent: "center" }}
+        >
+          <Button onClick={() => setShowModal(!showModal)}>
+            {showModal ? "Close Form" : "Add New Cabin"}
+          </Button>
+        </div>
+        {showModal && (
+          <Modal closeModal={closeModal}>
+            <CreateCabinForm closeModal={closeModal} />
+          </Modal>
+        )}
+      </div>
+    );
 }
 export default AddCabin
