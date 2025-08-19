0e4914a4e6e68ccf3e3d3558cb0410fbc7e691e1
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-19 11:14:16 +0100

    Update the AddCabin Component To Use the Modal Component and Other Properties

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,26 +1,17 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
 const AddCabin = () => {
-    const [showModal, setShowModal] = useState(false);
-    const closeModal = () => setShowModal(false);
-    return (
-      <div>
-        <div
-          style={{ width: "100%", display: "flex", justifyContent: "center" }}
-        >
-          <Button onClick={() => setShowModal(!showModal)}>
-            {showModal ? "Close Form" : "Add New Cabin"}
-          </Button>
-        </div>
-        {showModal && (
-          <Modal closeModal={closeModal}>
-            <CreateCabinForm closeModal={closeModal} />
-          </Modal>
-        )}
-      </div>
-    );
-}
-export default AddCabin
+  return (
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+    </Modal>
+  );
+};
+export default AddCabin;
