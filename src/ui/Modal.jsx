83f5231f33535bdcbfccc148299e38bfcbc1a58f
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-19 09:41:17 +0100

    Update Modal to Use CreatePortal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
 
@@ -53,7 +54,7 @@ const Button = styled.button`
 `;
 
 function Modal({ children, closeModal }) {
-  return (
+  return createPortal(
     <Overlay>
       <StyledModal>
         <Button onClick={closeModal}>
@@ -61,7 +62,8 @@ function Modal({ children, closeModal }) {
         </Button>
         <div>{children}</div>
       </StyledModal>
-    </Overlay>
+    </Overlay>,
+    document.body
   );
 }
 
