7c6abaf2e05de4d08c6b3f4b4659a9d24514890b
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-19 14:42:05 +0100

    Update the AddCabin Component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -3,15 +3,17 @@ import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
 const AddCabin = () => {
-  return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-    </Modal>
-  );
+    return (
+      <div>
+        <Modal>
+          <Modal.Open opens="cabin-form">
+            <Button>Add new cabin</Button>
+          </Modal.Open>
+          <Modal.Window name="cabin-form">
+            <CreateCabinForm />
+          </Modal.Window>
+        </Modal>
+      </div>
+    );
 };
 export default AddCabin;
