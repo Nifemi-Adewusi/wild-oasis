73d779558c9ee041a02f430dbac0a3425bad0ac6
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-21 17:12:26 +0100

    Update CabinTableOperations to include and accomodate the SortBy Component for better User Experience

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,27 +1,46 @@
 import Filter from '../../ui/Filter'
-import TableOperations from '../../ui/TableOperations'
-
+import SortBy from "../../ui/SortBy";
+import TableOperations from "../../ui/TableOperations";
 
 const options = [
   {
     type: "all",
     value: "All",
-    },
-    {
-        type: "no-discount",
-        value:"No Discount"
-    },
-    {
-        type:"with-discount",
-        value:"With Discount"
-    }
+  },
+  {
+    type: "no-discount",
+    value: "No Discount",
+  },
+  {
+    type: "with-discount",
+    value: "With Discount",
+  },
+];
+
+const sortOptions = [
+  { value: "name-asc", label: "Sort By Name(A-Z)" },
+  { value: "name-desc", label: "Sort By Name(Z-A)" },
+  {
+    value: "regularPrice-asc",
+    label: "Sort By Price (Low Price to High)",
+  },
+  {
+    value: "regularPrice-desc",
+    label: "Sort By Price (High Price to Low",
+  },
+  { value: "maxCapacity", label: "Sort By Capacity (High to Low)" },
+  {
+    value: "minCapacity",
+    label: "Sort By Capacity (Low to High)",
+  },
 ];
 function CabinTableOperations() {
-    return (
-        <TableOperations>
-            <Filter filterKey="discount" options={options}/>
-        </TableOperations>
-    )
+  return (
+    <TableOperations>
+      <Filter filterKey="discount" options={options} />
+      <SortBy filterKey="sortBy" options={sortOptions} />
+    </TableOperations>
+  );
 }
 
 export default CabinTableOperations
