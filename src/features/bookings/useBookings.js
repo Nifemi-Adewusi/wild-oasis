59e901940fe75b94f77c8794cb6b8832c453d7c9
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-08-25 03:33:10 +0100

    Update useBookings to handle sorting and filtering on the server side, also, include the necessary values

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
-    const { data: bookings, isLoading, error } = useQuery({
-        queryKey: ["bookings"],
-        queryFn:getBookings
-    })
-    return {bookings, isLoading, error}
+  const [searchParams] = useSearchParams();
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
+  const sortByRaw = searchParams.get("date");
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+  const {
+    data: bookings,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
+  });
+  return { bookings, isLoading, error };
 }
