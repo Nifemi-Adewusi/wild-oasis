152b25417295f2722ac226f6af1b12604fac8096
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-09-03 10:58:44 +0100

    Update useBookings

--- src/features/bookings/useBooking.js
+++ src/features/bookings/useBooking.js
@@ -6,12 +6,12 @@ import { useParams } from "react-router-dom";
 export function useBooking() {
     const {bookingId} = useParams()
     const {
-        data: booking,
-        isLoading,
-        error
+      data: booking,
+      isLoading,
+      error,
     } = useQuery({
-        queryKey: ["booking"],
-        queryFn: ()=> getBooking(bookingId)
-    })
+      queryKey: ["booking", bookingId],
+      queryFn: () => getBooking(bookingId),
+    });
     return { booking, isLoading, error }
 }
