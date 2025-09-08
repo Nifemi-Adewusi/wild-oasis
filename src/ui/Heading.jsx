82f52fb1a3e9862a2164bafa1132e7d7bcf4e66e
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-09-08 13:37:13 +0100

    Update Heading

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -2,24 +2,37 @@ import styled, { css } from "styled-components";
 
 // const test =  css`text-align:center`
 const Heading = styled.h1`
-${(props)=> props.as === "h1" && css`
-    
-    font-weight: 700;
-    /* background-color: yellow; */
-    font-size: 25px;
-`}
-${(props)=> props.as === "h2" && css`
-    font-weight: 600;
-    background-color: green;
-    font-size:20px;
-    width: 50%;
-    text-align: center;
-`}
-${(props)=> props.as === "h3" && css`
-    font-weight: 500;
-    background-color: aliceblue;
-    font-size: 15px;
-`}
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-weight: 700;
+      /* background-color: yellow; */
+      font-size: 25px;
+    `}
+  ${(props) =>
+    props.as === "h2" &&
+    css`
+      font-weight: 600;
+      background-color: green;
+      font-size: 20px;
+      width: 50%;
+      text-align: center;
+    `}
+${(props) =>
+    props.as === "h3" &&
+    css`
+      font-weight: 500;
+      background-color: aliceblue;
+      font-size: 15px;
+    `}
+${(props) =>
+    props.as === "h4" &&
+    css`
+      font-weight: 600;
+
+      font-size: 3rem;
+      text-align: center;
+    `}
   padding: 1rem;
   border-radius: 0.5rem;
   font-weight: 600;
