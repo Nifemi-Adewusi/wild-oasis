4e0b4b619fc60100e5e55c81ac77d5d3bf059923
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-09-08 18:01:52 +0100

    Update ProtectedRoute to use the authentication feature and redirect to login if users aren't currently logged in

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,7 +1,42 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 /* eslint-disable react/prop-types */
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 function ProtectedRoute({ children }) {
-    
-    return {children}
+  const navigate = useNavigate();
+  const { isAuthenticated, isLoading } = useUser();
+
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) {
+        navigate("/login");
+      }
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+  if (isLoading) {
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+  }
+
+  //   return { children };
+  if (isAuthenticated) {
+    return children;
+  }
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
