35256e16b53d341136f85570fa51e68a3656670c
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-09-09 06:21:43 +0100

    Update LoginForm

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -16,7 +16,15 @@ function LoginForm() {
   function handleSubmit(e) {
     e.preventDefault();
     if (!email || !password) return;
-    login({ email, password });
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setPassword("");
+          setEmail("");
+        },
+      }
+    );
   }
 
   if (isLoading) {
