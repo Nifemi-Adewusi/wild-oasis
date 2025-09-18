bee901ba56d6e25d1f4695d43b4b287c33b4d748
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-09-18 15:33:18 +0100

    Update UserAvatar

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useUser } from "./useUser";
+import defaultUserImage from "../../data/img/default-user.jpg";
 
 const StyledUserAvatar = styled.div`
   display: flex;
@@ -19,3 +21,16 @@ const Avatar = styled.img`
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
 `;
+
+function UserAvatar() {
+  const { user } = useUser();
+
+  const { fullName, avatar } = user.user_metadata;
+
+  <StyledUserAvatar>
+    <Avatar src={avatar || defaultUserImage} alt={fullName} />
+    <span>{fullName}</span>
+  </StyledUserAvatar>;
+}
+
+export default UserAvatar;
