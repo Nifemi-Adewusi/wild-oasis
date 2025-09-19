bf648484f5c2809066c3bbf8142b0ad318f62abb
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-09-19 16:44:13 +0100

    Update Header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -8,6 +8,10 @@ const StyledHeader = styled.header`
 
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 function Header() {
