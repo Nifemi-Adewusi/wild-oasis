ea8c3b5fd15efbf9cb82cbce3e65d8a8a624fb5e
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-09-19 16:45:13 +0100

    Include feature to update the user avatar and other credentials

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import { data } from "autoprefixer";
 import supabase from "./supabase";
+// import { supabaseUrl } from "./config";
+import { supabaseUrl } from "./supabase";
 
 export async function login({ email, password }) {
   let { data, error } = await supabase.auth.signInWithPassword({
@@ -38,4 +40,38 @@ export async function signup({ fullName, email, password }) {
       },
     },
   });
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // Update the password or fullName or avatar.
+
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+  if (avatar) updateData = { data: { avatar } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return;
+
+  // Upload Avatar Image
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // Update avatar in the user
+  const { data: updatedUser, error: updateError } =
+    await supabase.auth.updateUser({
+      // avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+
+  if (updateError) throw new Error(updateError.message);
+  return updatedUser;
 }
