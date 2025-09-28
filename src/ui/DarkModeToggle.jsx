a983e22de383b0c525a22e1d90629def36eecb3e
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-09-28 08:19:45 +0100

    Update DarkModeToggle

--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -1,3 +1,12 @@
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
+import ButtonIcon from "./ButtonIcon";
+import { useDarkMode } from "../context/DarkModeContext";
+
 export default function DarkModeToggle() {
-  return <div>DarkModeToggle</div>;
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
+  return (
+    <ButtonIcon onClick={toggleDarkMode}>
+      {isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
+    </ButtonIcon>
+  );
 }
