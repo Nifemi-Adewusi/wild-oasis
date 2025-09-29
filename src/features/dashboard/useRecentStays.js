9ac464822a979b796e09a0bba3cbb89cd5e1ecad
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-09-29 11:27:35 +0100

    Update custom hook to return the number of confirmedStays

--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -12,5 +12,9 @@ export function useRecentStays() {
     queryFn: () => getStaysAfterDate(queryDate),
     queryKey: ["bookings", `last-${numDays}`],
   });
-  return { isLoading, stays };
+
+  const confirmedStays = stays?.filter(
+    (stay) => stay.status === "checked-in" || stay.status === "checked-out"
+  );
+  return { isLoading, stays, confirmedStays };
 }
