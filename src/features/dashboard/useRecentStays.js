1491b0065dbdcf613a59b2f99b7d048a016df37e
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-03 07:55:34 +0100

    Update the custom hook useRecentStays

--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -16,5 +16,5 @@ export function useRecentStays() {
   const confirmedStays = stays?.filter(
     (stay) => stay.status === "checked-in" || stay.status === "checked-out"
   );
-  return { loadingStays, stays, confirmedStays };
+  return { loadingStays, stays, confirmedStays, numDays };
 }
