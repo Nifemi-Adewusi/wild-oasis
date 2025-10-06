79aa0c55432b2bdc20641912f6d4e9fb7c4c302b
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-06 12:06:34 +0100

    Update the supabase file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://magvjnsrhwpghodbgpqc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hZ3ZqbnNyaHdwZ2hvZGJncHFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1ODcxNTgsImV4cCI6MjA3MDE2MzE1OH0._r5lgGFrhQkTyBYUoa7qwr2SfKU90g-KKyRV64ST6UI";
+// export const supabaseUrl = "https://magvjnsrhwpghodbgpqc.supabase.co";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
