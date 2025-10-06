eeeac34120af5a49fab0d3f24a25ffd29b720d75
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-06 12:13:17 +0100

    Update checkoutButton

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,16 @@
+/* eslint-disable react/prop-types */
 import Button from "../../ui/Button";
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { checkout, isCheckingOut } = useCheckout();
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="size"
+      onClick={() => checkout(bookingId)}
+      disabled={isCheckingOut}
+      size="small"
+    >
       Check out
     </Button>
   );
